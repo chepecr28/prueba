77c71f9ce97422be83ead73ef4cc8225e6f394c1
Chepecr28 <161408586+chepecr28@users.noreply.github.com>
2025-02-24 22:46:52 -0600

    ponienome al dia

---
+++ src/main/java/com/prueba/controller/IndexController.java
@@ -0,0 +1,28 @@
+
+package com.prueba.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author joseg
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("attribute", "value");
+        return "index";
+    }
+    
+    @RequestMapping("/informacion")
+    public String contacto(Model model) {
+      
+        return "contacto";
+    }
+    
+
+}
