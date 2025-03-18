d6e211edc88e73ca8fe11e7c1d94fb0452219e00
Chepecr28 <161408586+chepecr28@users.noreply.github.com>
2025-03-17 22:23:47 -0600

    otro

--- src/main/java/com/prueba/ServiceImpl/CategoriaServiceImpl.java
+++ src/main/java/com/prueba/ServiceImpl/CategoriaServiceImpl.java
@@ -7,7 +7,7 @@ import com.prueba.service.CategoriaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
 public class CategoriaServiceImpl implements CategoriaService{
--- src/main/java/com/prueba/dao/CategoriaDao.java
+++ src/main/java/com/prueba/dao/CategoriaDao.java
@@ -5,7 +5,7 @@
 
 package com.prueba.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.prueba.domain.Categoria;
 
     public interface CategoriaDao extends JpaRepository<Categoria, Long>{
