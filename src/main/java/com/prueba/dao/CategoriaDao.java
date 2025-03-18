c9c6a1dbf8a3ebe3351f413d55d0ce12d95221c2
Chepecr28 <161408586+chepecr28@users.noreply.github.com>
2025-03-17 21:26:50 -0600

    semana_5

---
+++ src/main/java/com/prueba/dao/CategoriaDao.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.prueba.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.Tienda.domain.Categoria;
+
+    public interface CategoriaDao extends JpaRepository<Categoria, Long>{
+    
+}
---
+++ src/main/java/com/prueba/domain/Categoria.java
@@ -0,0 +1,42 @@
+
+
+package com.prueba.domain;
+
+import lombok.Data;
+import java.io.Serializable;
+import jakarta.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+@Table (name="categoria")
+public class Categoria implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "id_categoria")
+    private Long idCategoria;
+    private String descripcion;
+    private String rutaImagen;
+    private boolean activo;
+    
+    @OneToMany
+    @JoinColumn(name = "id_Categoria", insertable = false, updatable = false)
+    private List<Producto> productos;
+    
+    public Categoria()
+    {
+    }
+    
+    public Categoria(String descripcion, String rutaImagen, boolean activo)
+    {
+        this.descripcion = descripcion;
+        this.rutaImagen = rutaImagen;
+        this.activo = activo;
+    }
+    
+    
+}
